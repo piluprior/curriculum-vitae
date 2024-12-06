f2f7b32599f8121621644cf90f5a94ca6bb9621e
Marc_LUPI <marc.lupi@protonmail.com>
2024-12-06 07:16:50 +0100

    ajouts et modifications

--- JS/scripts.js
+++ JS/scripts.js
@@ -46,3 +46,11 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     });
 });
+
+    // initialise tous les tooltips présents sur la page
+    document.addEventListener('DOMContentLoaded', function () {
+        var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
+        var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+            return new bootstrap.Tooltip(tooltipTriggerEl);
+        });
+    });
